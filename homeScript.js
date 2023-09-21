21c0a347a0fb2fae170c371c1c483bc6919a282c
RajpalSiya <srajp10@gmail.com>
2023-09-20 22:20:47 -0600

    finished preliminary website

--- homeScript.js
+++ homeScript.js
@@ -1,36 +1,5 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-const modal1 = document.getElementById('card-review');
-const modalButton1 = document.getElementById('openModalButton1');
-const closeModalButton1 = document.getElementById('closeModal');
-
-
-function openModal() {
-    modal1.style.display = 'inline';
+function openCustomModal() {
+    $('#review-modal-1').modal('show');
 }
-
-function closeModal() {
-    modal1.style.display = 'none';
-}
-
-modalButton1.addEventListener('click', openModal);
-closeModalButton1.addEventListener('click', closeModal);
-
-
-window.addEventListener('click', (e) => {
-    if(e.target == modal1) {
-        closeModal();
-    }
-})
-
-
-const modalButton2 = document.getElementById('openModalButton2');
-const modal2 = new bootstrap.Modal(document.getElementById('testModal'));
-
-function openModal2() {
-    modal2.show();
-}
-
-modalButton2.addEventListener('click', openModal2);
